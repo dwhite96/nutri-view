f070db249e231254c7798337b3b7a7efd7936538
David White <whited75@gmail.com>
2020-01-20 19:00:05 -0700

    spec/rails_helper.rb

--- spec/factories/food_items.rb
+++ spec/factories/food_items.rb
@@ -1,5 +1,32 @@
 FactoryBot.define do
   factory :food_item do
-    
+    data {
+      "foodClass": "Branded",
+      "description": "ORGANIC PLANT PROTEIN PEANUT BUTTER DARK CHOCOLATE CHIP CHEWY BARS",
+      "foodNutrients": [
+        {
+          "type": "FoodNutrient",
+          "id": 7332240,
+          "nutrient": {
+            "id": 1003,
+            "number": "203",
+            "name": "Protein",
+            "rank": 600,
+            "unitName": "g"
+          },
+          "foodNutrientDerivation": {
+            "id": 70,
+            "code": "LCCS",
+            "description": "Calculated from value per serving size measure",
+            "foodNutrientSource": {
+              "id": 9,
+              "code": "12",
+              "description": "Manufacturer's analytical; partial documentation"
+            }
+          },
+          "amount": 24.00000000
+        }
+      ]
+    }
   end
 end
