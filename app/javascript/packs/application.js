ba41792cff06049d8fe7be72712bebc81fe5c6a4
David White <whited75@gmail.com>
2020-02-04 18:14:38 -0700

    Add foundation css framework

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import './nutri-view-bundle';
+import 'foundation-sites/dist/css/foundation.min.css';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
