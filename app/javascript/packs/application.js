c2a385752c64aeb5eeee28ebeb596826c21b93f6
David White <whited75@gmail.com>
2020-04-07 19:10:49 -0600

    Remove unnecessary nutri-view-bundle.js import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import 'foundation-sites/dist/css/foundation.min.css';
-import './nutri-view-bundle';
 
 require('@rails/ujs').start();
 require('turbolinks').start();
