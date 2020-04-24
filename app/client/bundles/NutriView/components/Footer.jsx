11484fac8ac3086da6e7f481483a451600a67f12
David White <whited75@gmail.com>
2020-04-24 15:25:41 -0600

    Make footer sticky to viewport

--- app/client/bundles/NutriView/components/Footer.jsx
+++ app/client/bundles/NutriView/components/Footer.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Layout, Typography } from 'antd';
+import { Layout, Typography, Affix } from 'antd';
 
 const { Footer } = Layout;
 const { Text } = Typography;
 
 const AppFooter = () => (
-  <Footer>
-    <Text>
-      Food Data Source: U.S. Department of Agriculture, Agricultural Research Service. FoodData Central, 2019.
-    </Text>
-    &nbsp;
-    <a
-      href="https://fdc.nal.usda.gov/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      fdc.nal.usda.gov
-    </a>
-  </Footer>
+  <Affix offsetBottom={0}>
+    <Footer>
+      <Text>
+        Food Data Source: U.S. Department of Agriculture, Agricultural Research Service. FoodData Central, 2019.
+      </Text>
+      &nbsp;
+      <a
+        href="https://fdc.nal.usda.gov/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        fdc.nal.usda.gov
+      </a>
+    </Footer>
+  </Affix>
 );
 
 export default AppFooter;
