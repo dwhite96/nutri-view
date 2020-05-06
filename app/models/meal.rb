b11c6721892921b7447b95ca1b9f7d615d54f257
David White <whited75@gmail.com>
2020-05-05 23:14:26 -0600

    Add Meal validations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,11 @@
 
 # Meal model
 class Meal < ApplicationRecord
+  validates :food_item, presence: true
+  validates :number,
+    presence:     true,
+    uniqueness:   true,
+    numericality: { only_integer: true, less_than: 11 }
+
   has_and_belongs_to_many :food_items
 end
