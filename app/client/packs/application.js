4dcd97c8bb9fc1a1c8aa603da83b2b3074446714
David White <whited75@gmail.com>
2020-09-09 21:37:33 -0600

    Add stylesheet extention to import statement

--- app/client/packs/application.js
+++ app/client/packs/application.js
@@ -7,12 +7,12 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import './nutri-view-bundle';
-import '../stylesheets/application';
+import '../stylesheets/application.scss';
 
 require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
-require('channels');
+// require('channels');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
